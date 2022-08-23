c5dd6bc0fb2cb58d0c546e48b98e1c2f11ee8491
SenchoDev <beagnovicsenadin24@gmail.com>
2022-08-23 23:21:55 +0200

    add playsFor function and changed perf parameter

--- code.js
+++ code.js
@@ -35,7 +35,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play)
     // add volume credits
     volumeCredits += Math.max(perf.audience > 30, 0);
@@ -51,26 +51,30 @@ function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
-  function amountFor(perf, play) {
+  function amountFor(aPerformance, play) {
     let result = 0;
     switch (play.type) {
       case "tragedy":
         result = 40000;
-        if (perf.audience > 30) {
-          result += 1000 * (perf.audience - 30);
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
         }
         break;
       case "comedy":
         result = 30000;
-        if (perf.audience > 20) {
-          result += 10000 + 500 * (perf.audience - 20);
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
         }
-        result += 300 * perf.audience;
+        result += 300 * aPerformance.audience;
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 
