49d366fb118fa28d3b542cfbc50a33dd281fc992
Senadin Beganovic <beganovicsenadin24@gmail.com>
2022-08-29 16:44:12 +0200

    Deep copied performances data

--- code.js
+++ code.js
@@ -26,13 +26,15 @@ const invoice = {
 
 function statement(invoice, plays) {
   let statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = [...invoice.performances];
   return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+  let result = `Statement for ${data.customer}\n`;
 
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
@@ -85,7 +87,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
@@ -93,7 +95,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
